412ca5999e6f7436e17c83ac922ada3a50b72e57
Unknown <nodecourse98@gmail.com>
2017-09-07 15:19:37 +0300

    added create function to constraint

--- src/manager/constraint.manager.js
+++ src/manager/constraint.manager.js
@@ -1,4 +1,21 @@
 var objectId = require('mongoose').Types.ObjectId;
-var User = require('../models/constraint.model');
+var Constraint = require('../models/constraint.model');
 var ConstraintValidation = require('validations/constraint.validator');
 
+function create(user, startDate, endDate) {
+    if(ConstraintValidation.dateRangeValidity(startDate, endDate)) {
+        
+        newConstraint = new Constraint({
+            user: ConstraintValidation.userValidity(user),
+            startDate: ConstraintValidation.dateValidity(startDate),
+            endDate: ConstraintValidation.dateValidity(endDate)
+        });
+
+        return newConstraint.save();
+    }
+    return null;
+}
+
+module.exports = {
+    create
+};
