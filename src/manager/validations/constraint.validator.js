ef62cca0838cab3390cc3a77a933960f605c0a95
Unknown <nodecourse98@gmail.com>
2017-09-07 16:55:35 +0300

    Added mission validation

--- src/manager/validations/constraint.validator.js
+++ src/manager/validations/constraint.validator.js
@@ -4,10 +4,6 @@ function userValidity(user) {
     return objectId.isValid(user) ? user : null;
 }
 
-/*function dateRangeValidity(startDate, endDate) {
-    return (startDate <= endDate);
-}*/
-
 function dateValidity(date) {
     return !isNaN(Date.parse(date)) ? date: new Date();
 }
---
+++ src/manager/validations/mission.validator.js
@@ -0,0 +1,37 @@
+var objectId = require('mongoose').Types.ObjectId;
+
+function typeValidity(type) {
+    return objectId.isValid(type) ? type : null;
+}
+
+function dateRangeValidity(startDate, endDate) {
+    return (startDate <= endDate);
+}
+
+function dateValidity(date) {
+    return !isNaN(Date.parse(date)) ? date: new Date();
+}
+
+function statusValidity(status) {
+    return !isNaN(status) ? status : 0;
+}
+
+function participentsValidity(participents) {
+    checked = [];
+
+    participents.forEach(function(participent) {
+        if(objectId.isValid(participent)){
+            checked.push(participent);
+        }
+    }, this);
+
+    return checked;
+}
+
+module.exports = {
+    typeValidity,
+    dateRangeValidity,
+    dateValidity,
+    statusValidity,
+    participentsValidity
+};
