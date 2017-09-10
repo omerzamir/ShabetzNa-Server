ceb2f5c517750ba9e32afeb4567273991359c6f6
Unknown <nodecourse98@gmail.com>
2017-09-10 17:09:12 +0300

    added functions to the controller

--- src/controller/missiontype.controller.js
+++ src/controller/missiontype.controller.js
@@ -1,1 +1,35 @@
-var userManager = require('../manager/missiontype.manager');
+var missionTypeManager = require('../manager/missiontype.manager');
+
+function createMissionType(name, description, type){
+    return missionTypeManager.create(name, description, type);
+}
+
+function getAllMissionTypes() {
+    return missionTypeManager.getAll();
+}
+
+function getMissionTypesByType(type) {
+    return missionTypeManager.getByType(type);    
+}
+
+function getMissionTypeById(id) {
+    return missionTypeManager.getById(id);        
+}
+
+function updateMissionType(id, name, description, type) {
+    return missionTypeManager.update(id, name, description, type);            
+}
+
+function DeleteMissionTypeById(id) {
+    return missionTypeManager.Delete(id);            
+    
+}
+
+module.exports = {
+    createMissionType,
+    getAllMissionTypes,
+    getMissionTypesByType,
+    getMissionTypeById,
+    updateMissionType,
+    DeleteMissionTypeById
+}
