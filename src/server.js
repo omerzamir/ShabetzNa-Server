7645c7df0770f806a3353a7698840280d9e17c09
Unknown <nodecourse98@gmail.com>
2017-09-11 11:59:18 +0300

    added bodyparser

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ var server = require('express')();
 var config = require('./config');
 var mongoose = require('mongoose');
 var apiRouter = require('./router');
+var bodyParser = require('body-parser');
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 
 apiRouter(server);
 
