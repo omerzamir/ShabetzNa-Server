910cad860b49b00610e0bd161dbd792757188422
Unknown <nodecourse98@gmail.com>
2017-09-11 11:25:40 +0300

    works now

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,87 +2,87 @@ var router          = require('express').Router();
 var userController  = require('../controller/user.controller');
 
 
-router.get('/', function(req,res) {
-    res.json(userController.getAllUsers());
+router.get('/',async function(req,res) {
+    res.json(await userController.getAllUsers());
 });
 
-router.get('/user/:userName',  function(req, res) {
-    res.json( userController.getUserByUsername(req.params.userName));
+router.get('/user/:userName', async function(req, res) {
+    res.json(await userController.getUserByUsername(req.params.userName));
 });
 
-router.post('/user',  function(req, res) {
-    res.json( userController.createUser(req.body.username, 
+router.post('/user', async function(req, res) {
+    res.json(await userController.createUser(req.body.username, 
                                         req.body.name, 
                                         req.body.userspermissions,
                                         req.body.specialpermissions,
                                         req.body.exemptions));
 });
 
-router.put('/user/permissions/user',  function(req, res) {
-    res.json( userController.updateAllUserPermission(
+router.put('/user/permissions/user', async function(req, res) {
+    res.json(await userController.updateAllUserPermission(
         req.body.username,
         req.body.userspermissions
     ));
 });
 
 
-router.put('/user/permissions/special',  function(req, res) {
-    res.json( userController.updateAllSpecialPermissions(
+router.put('/user/permissions/special', async function(req, res) {
+    res.json(await userController.updateAllSpecialPermissions(
         req.body.username,
         req.body.specialpermissions
     ));
 });
 
-router.put('/user/exemptions',  function(req, res) {
-    res.json( userController.updateAllExemptions(
+router.put('/user/exemptions', async function(req, res) {
+    res.json(await userController.updateAllExemptions(
         req.body.username,
         req.body.exemptions
     ));
 });
 
-router.put('/user/permission/user',  function(req, res) {
-    res.json( userController.addUserPermission(
+router.put('/user/permission/user', async function(req, res) {
+    res.json(await userController.addUserPermission(
         req.body.username,
         req.body.usernameToAddName
     ));
 });
 
-router.put('/user/permission/special',  function(req, res) {
-    res.json( userController.addSpecialPermission(
+router.put('/user/permission/special', async function(req, res) {
+    res.json(await userController.addSpecialPermission(
         req.body.username,
         req.body.specialPermission
     ));
 });
 
-router.put('/user/exemption',  function(req, res) {
-    res.json( userController.addExemption(
+router.put('/user/exemption', async function(req, res) {
+    res.json(await userController.addExemption(
         req.body.username,
         req.body.exemption
     ));
 });
 
-router.delete('/user',  function(req, res) {
-    res.json( userController.DeleteUser(
+router.delete('/user', async function(req, res) {
+    res.json(await userController.DeleteUser(
         req.body.username
     ));
 });
 
-router.delete('/user/exemption',  function(req, res) {
-    res.json( userController.removeOneExempt(
+router.delete('/user/exemption', async function(req, res) {
+    res.json(await userController.removeOneExempt(
         req.body.username,
         req.body.exemption
     ));
 });
 
-router.delete('/user/permission/special',  function(req, res) {
-    res.json( userController.removeOneSpecialPermission(
+router.delete('/user/permission/special', async function(req, res) {
+    res.json(await userController.removeOneSpecialPermission(
         req.body.username,
         req.body.specialPermission
     ));
 });
 
-router.delete('/user/permission/user',  function(req, res) {
-    res.json( userController.removeOneUserPermission(
+router.delete('/user/permission/user', async function(req, res) {
+    res.json(await userController.removeOneUserPermission(
         req.body.username,
         req.body.usernameToRemove
     ));
