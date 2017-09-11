ee7b38e936e0ce957e83ac610532fe4a5003e0ae
Unknown <nodecourse98@gmail.com>
2017-09-11 15:08:33 +0300

    added route to global router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 userRouter = require('./routes/user.router');
 missionTypeRouter = require('./routes/missiontype.router');
+missionRouter = require('./routes/mission.router');
 
 module.exports = (server) => {
     server.use('/api/user', userRouter);
     server.use('/api/missiontype', missionTypeRouter);
+    server.use('/api/mission', missionRouter);
 }
