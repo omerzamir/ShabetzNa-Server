dde8469c4ed4920ef3451ed21bc4718598758e09
Unknown <nodecourse98@gmail.com>
2017-09-27 13:11:40 +0300

    added jenkins.groovy file

---
+++ itamar.groovy
@@ -0,0 +1,15 @@
+def gitUrl = 'https://github.com/nodecourse98/ShabetzNa-Server'
+
+job('ShabetzNa-Unit-Testing') {
+    scm {
+        git(gitUrl)
+    }
+    triggers {
+        scm('*/5 * * * *')
+    }
+    steps {
+        shell('cd /var/lib/jenkins/workspace/ShabetzNa')
+        shell('npm install')
+        shell('npm test')
+    }
+}
